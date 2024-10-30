6be044f5568304210c40df2f3407320a5b3bf5e1
Pulkio <guillaume.cobat@gmx.fr>
2024-10-30 23:08:51 +0100

    test mobile

--- scripts/loisirs.js
+++ scripts/loisirs.js
@@ -4,13 +4,13 @@ let currentSection = 0;
 function scrollToSection(section) {
     scrollContainer.children[section].scrollIntoView({ behavior: 'smooth' });
 }
+
 let isScrolling = false;
+let startY = 0;
 
 window.addEventListener('wheel', (event) => {
     if (isScrolling) return;
     isScrolling = true;
-
-    console.log(isScrolling)
 
     if (event.deltaY > 0) {
         currentSection = Math.min(currentSection + 1, scrollContainer.children.length - 1);
@@ -18,10 +18,35 @@ window.addEventListener('wheel', (event) => {
         currentSection = Math.max(currentSection - 1, 0);
     }
 
-    console.log(currentSection)
     scrollToSection(currentSection);
 
     setTimeout(() => {
         isScrolling = false;
     }, 300);
+});
+
+window.addEventListener('touchstart', (event) => {
+    startY = event.touches[0].clientY;
+});
+
+window.addEventListener('touchmove', (event) => {
+    if (isScrolling) return;
+    isScrolling = true;
+
+    const currentY = event.touches[0].clientY;
+    const deltaY = currentY - startY;
+
+    if (deltaY > 0) {
+        currentSection = Math.min(currentSection + 1, scrollContainer.children.length - 1);
+    } else {
+        currentSection = Math.max(currentSection - 1, 0);
+    }
+
+    scrollToSection(currentSection);
+
+    setTimeout(() => {
+        isScrolling = false;
+    }, 300);
+
+    startY = currentY;
 });
